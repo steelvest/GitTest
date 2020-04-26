ba08e7f5148f4b3456ffe6a5f9af22cc914ba643
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-26 05:39:24 +0300

    diziler eklendi

--- KentSimgeleri/ViewController.swift
+++ KentSimgeleri/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        
+        var sehirIsimleri = [String]()
+        sehirIsimleri.append("Aksaray")
+        sehirIsimleri.append("Adana")
+        sehirIsimleri.append("Şanlıurfa")
+        sehirIsimleri.append("İstanbul")
+        
+        var sehirResimleri = [UIImage]()
+        sehirResimleri.append(UIImage(named: "aksaray")!)
+        sehirResimleri.append(UIImage(named: "adana")!)
+        sehirResimleri.append(UIImage(named: "urfa")!)
+        sehirResimleri.append(UIImage(named: "istanbul")!)
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
