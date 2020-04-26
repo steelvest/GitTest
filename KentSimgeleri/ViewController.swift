b73d90ab709800d708266451e2fff539dc336069
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-26 05:06:43 +0300

    Initial Commit

---
+++ KentSimgeleri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KentSimgeleri
+//
+//  Created by Oğuz Özgül on 26.04.2020.
+//  Copyright © 2020 Oğuz Özgül. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
