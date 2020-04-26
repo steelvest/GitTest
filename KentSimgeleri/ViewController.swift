2590b63fbc887b69d56c39020f0872947dcb6a81
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-26 06:23:50 +0300

    Navigation Title Eklendi

--- KentSimgeleri/ViewController.swift
+++ KentSimgeleri/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         sehirResimleri.append(UIImage(named: "adana")!)
         sehirResimleri.append(UIImage(named: "urfa")!)
         sehirResimleri.append(UIImage(named: "istanbul")!)
+        navigationItem.title = "Şehir Resimleri"
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
